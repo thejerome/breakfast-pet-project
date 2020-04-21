adb7860c0177b8efe87afa29c9b5027e796cc1aa
thejerome <thejerome@mail.ru>
2020-04-21 10:35:49 +0300

    Initial commit. Hello World endpoint

---
+++ src/main/java/com/epam/demo/breakfastpetproject/BreakfastPetProjectApplication.java
@@ -0,0 +1,13 @@
+package com.epam.demo.breakfastpetproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakfastPetProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreakfastPetProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epam/demo/breakfastpetproject/BreakfastPetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.demo.breakfastpetproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreakfastPetProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
